ebb9b55f15cdb7b8bcea56af35d14d6b89b9a20e
Rupesh Jaiswal <rupeshjaiswal@openvpn-client.vpn.dss.husqvarnagroup.com>
2018-03-09 12:54:23 +0530

    Added Hello World Branch

--- gittest/ViewController.swift
+++ gittest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello world master")
+        print("hello world branch")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
